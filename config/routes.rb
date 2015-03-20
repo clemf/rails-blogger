fe2a3dd3c8693b408da51269c4cb15014e8ac8ff
Clem Freeman <clemfree@gmail.com>
2015-03-20 13:48:28 -0700

    Add posts index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'users#index'
-  resources :users
+  resources :users do
+    resources :posts
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
