7ea284242341adcf6bc34328fc787efea44062bc
Clem Freeman <clemfree@gmail.com>
2015-03-20 14:45:29 -0700

    Add new posts view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post created.'
+      redirect_to user_posts_path
+    else
+      render :new
+    end
+  end
+  
+private
+  def post_params
+    params.require(:post).permit(:title, :text, :user_id)
+  end
 end
