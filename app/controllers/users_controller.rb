ceb8ea24ba18af7880503bd01620c029a85a5d44
Clem <clemfree@gmail.com>
2015-03-27 11:29:36 -0700

    Add Devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,6 @@ class UsersController < ApplicationController
 
 private
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
