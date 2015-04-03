9b65669afda72603e1f7d27eb4a31c7d8b36c984
Clem Freeman <clemfree@gmail.com>
2015-04-03 11:22:07 -0700

    Pass log in feature test

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,7 @@
 FactoryGirl.define do
-  
+  factory :user do
+    email "test@test.com"
+    username "testuser"
+    password "password"
+  end
 end
--- spec/features/log_in_user_spec.rb
+++ spec/features/log_in_user_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 feature 'the log in a user process' do
   scenario 'logs in a user with correct credentials' do
     visit root_path
-    click_on 'Sign in'
+    create(:user)
+    click_link 'Sign in'
     fill_in 'Username', with: 'testuser'
     fill_in 'Password', with: 'password'
-    click_on 'Sign in'
-    expect(page).to have_content 'signed in'
+    click_button 'Log in'
+    expect(page).to have_content 'Signed in'
   end
 end
